1c6db46b0ffa46ef636b1c2863e5d6da8272a92b
Aidan Pincin <93996840+AidanPincin@users.noreply.github.com>
2022-08-18 11:58:50 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,64 +1,52 @@
 import { grabImage } from "./images.js"
-import { getMoves, checkPos, movePiece, ifCheckmate, RecordMove } from "./pieceMoves.js"
+import { copyObject, getMoves, copyArray, movePiece, checkPos, ifCheck, ifCheckmate , recordMove } from "./pieceMoves.js"
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 canvas.height = window.document.defaultView.innerHeight-20
 canvas.width = window.document.defaultView.innerWidth-20
-const cooldown = 100
-function size(){
-    let length;
-    if(canvas.width>canvas.height){
-        length = canvas.height
-        if(canvas.width-canvas.height<200){
-            length = canvas.width-200
-        }
-    }
-    else{
-        length = canvas.width
-        if(canvas.height-canvas.width<200){
-            length = canvas.height-200
-        }
-    }
-    if(length<400){
-        length = 400
-    }
-    return length
-}
-const pieceValues = {
+const values = {
     'Pawn': 1,
-    'Queen': 9,
-    'King': 10,
-    'Rook': 5,
     'Knight': 3,
-    'Bishop': 3
+    'Bishop': 3,
+    'Rook': 5,
+    'Queen': 9,
+    'King': 10
 }
 class Piece{
     constructor(color,name,pos){
         this.color = color
         this.name = name
         this.pos = pos
-        this.img = grabImage(color+name)
-        this.value = pieceValues[name]
+        this.value = values[name]
     }
-    draw(x,y,w,h){
-        ctx.drawImage(this.img,x+this.pos.col*w/8,y+this.pos.row*h/8,w/8,h/8)
+    draw(margin,size){
+        ctx.drawImage(grabImage(this.color+this.name),margin+this.pos.col*size/8,margin+this.pos.row*size/8,size/8,size/8)
     }
-    updatePiece(name){
-        this.img = grabImage(this.color+name)
-        this.value = pieceValues[name]
+    update(name){
+        this.name = name
+    }
+    moves(state,moveHistory){
+        return getMoves(this,state,moveHistory)
+    }
+    move(pos,state,moveHistory){
+        movePiece(this,pos,state,moveHistory)
+        this.moving = true
+        for(let i=0; i<50; i++){
+            setTimeout(() => {
+                this.pos.row += (pos.row-this.pos.row)/200*i
+                this.pos.col += (pos.col-this.pos.col)/200*i
+            },i*10)
+        }
+        setTimeout(() => {
+            this.pos = pos
+            this.moving = false
+        },500)
     }
 }
-class ChessBoard{
-    constructor(x,y,w,h){
-        this.x = x
-        this.y = y
-        this.w = w
-        this.h = h
-        this.turn = 'white'
-        this.pieces = this.boardSetup()
-        this.selectedPiece = undefined
-        this.moveHistory = []
-        //setTimeout(() => {bot2.play(this.pieces,this.moveHistory)},cooldown)
+class Chessboard{
+    constructor(){
+        this.updateSize()
+        this.setup()
     }
     draw(){
         const oddNums = [1,3,5,7,9,11,13,15]
@@ -70,91 +58,99 @@ class ChessBoard{
                 else{
                     ctx.fillStyle = 'beige'
                 }
-                ctx.fillRect(this.x+col*this.w/8,this.y+row*this.h/8,this.w/8,this.h/8)
+                ctx.fillRect(this.margin+col*this.size/8,this.margin+row*this.size/8,this.size/8,this.size/8)
             }
         }
-        for(let i=0; i<this.pieces.length; i++){
-            this.pieces[i].draw(this.x,this.y,this.w,this.h)
+        ctx.lineWidth = 2
+        ctx.strokeRect(this.margin-1,this.margin-1,this.size+2,this.size+2)
+        const king = this.pieces.find(p => p.color == this.turn && p.name == 'King')
+        if(ifCheck(king,king.pos,this.pieces,this.moveHistory) && this.pieces.find(p => p.moving == true) == undefined){
+            const pos = this.convertPos(king.pos)
+            pos.x -= this.size/16
+            pos.y -= this.size/16
+            ctx.fillStyle = '#ff0000'
+            ctx.fillRect(pos.x,pos.y,this.size/8,this.size/8)
         }
-        if(this.selectedPiece != undefined){
-            const moves = getMoves(this.selectedPiece,this.pieces,this.moveHistory)
+        this.pieces.forEach(p => p.draw(this.margin,this.size))
+        if(this.selectedPiece != undefined && chessboard.turn == 'white'){
+            const moves = this.selectedPiece.moves(this.pieces,this.moveHistory)
             for(let i=0; i<moves.length; i++){
-                if(this.pieces.find(p => checkPos(p.pos,moves[i]) && p != this.selectedPiece) || (this.selectedPiece.name == 'Pawn' && moves[i].col != this.selectedPiece.pos.col)){
+                const pos = this.convertPos(moves[i])
+                if(this.pieces.find(p => checkPos(p.pos,moves[i])) != undefined || (this.selectedPiece.name == 'Pawn' && moves[i].col != this.selectedPiece.pos.col)){
                     ctx.fillStyle = '#ff0000'
                 }
                 else{
                     ctx.fillStyle = '#00ff00'
                 }
                 ctx.beginPath()
-                ctx.arc(this.x+moves[i].col*this.w/8+this.w/16,this.y+moves[i].row*this.h/8+this.h/16,this.w/64,0,Math.PI*2,false)
+                ctx.arc(pos.x,pos.y,this.size/64,0,Math.PI*2,false)
                 ctx.fill()
             }
         }
     }
-    boardSetup(){
-        const pieces = []
+    updateSize(){
+        this.size = Math.min(...[canvas.width,canvas.height])*0.875
+        this.margin = this.size/14
+    }
+    setup(){
+        this.pieces = []
+        this.turn = 'white'
+        this.moveHistory = []
         const colors = ['black','white']
-        const pieceNames = ['Rook','Knight','Bishop']
-        for(let color=0; color<2; color++){
-            pieces.push(new Piece(colors[color],'Queen',{row: color*7, col: 3}))
-            pieces.push(new Piece(colors[color],'King',{row: color*7, col: 4}))
-            for(let col=0; col<2; col++){
-                for(let name=0; name<pieceNames.length; name++){
-                    pieces.push(new Piece(colors[color],pieceNames[name],{row: color*7, col: name+col*(7-name*2)}))
-                }
+        for(let i=0; i<colors.length; i++){
+            for(let p=0; p<8; p++){
+                this.pieces.push(new Piece(colors[i],'Pawn',{row:1+i*5,col:p}))
             }
-        }
-        for(let col=0; col<8; col++){
-            pieces.push(new Piece('black','Pawn',{row: 1, col: col}))
-            pieces.push(new Piece('white','Pawn',{row: 6, col: col}))
-        }
-        return pieces
-    }
-    update(){
-        this.x = size()/16
-        this.y = size()/16
-        this.w = size()*0.875
-        this.h = size()*0.875
-    }
-    getPos(x,y){
-        for(let row=0; row<8; row++){
-            for(let col=0; col<8; col++){
-                if(x>=this.x+col*this.w/8 && x<=this.x+(col+1)*this.w/8 && y>=this.y+row*this.h/8 && y<=this.y+(row+1)*this.h/8){
-                    return { row: row, col: col }
-                }
+            for(let p=0; p<2; p++){
+                this.pieces.push(new Piece(colors[i],'Rook',{row:i*7,col:p*7}))
+                this.pieces.push(new Piece(colors[i],'Knight',{row:i*7,col:1+p*5}))
+                this.pieces.push(new Piece(colors[i],'Bishop',{row:i*7,col:2+p*3}))
             }
+            this.pieces.push(new Piece(colors[i],'Queen',{row:i*7,col:3}))
+            this.pieces.push(new Piece(colors[i],'King',{row:i*7,col:4}))
         }
-        return { row: 100, col: 100 }
     }
     wasClicked(e){
-        if(e.which == undefined || this.turn == 'white'){
-            const x = e.pageX-10
-            const y = e.pageY-10
-            const pos = this.getPos(x,y)
-            if(this.selectedPiece != undefined){
-                const moves = getMoves(this.selectedPiece,this.pieces,this.moveHistory)
-                if(moves.find(move => checkPos(move,pos))){
-                    if(this.turn == 'black'){
-                        this.turn = 'white'
-                        //setTimeout(() => {bot2.play(this.pieces,this.moveHistory)},cooldown)
-                    }
-                    else{
-                        this.turn = 'black'
-                        setTimeout(() => {bot1.play(this.pieces,this.moveHistory)},cooldown)
-                    }
-                    movePiece(this.selectedPiece,pos,this.pieces,this.moveHistory)
-                    this.selectedPiece = undefined
-                    const checkmate = ifCheckmate(this.pieces,this.moveHistory,this.turn)
-                    if(checkmate == true){
-                        setTimeout(() => {alert(['white','black'].find(c => c != this.turn)+' wins!')},100)
-                    }
+        const x = e.pageX-10
+        const y = e.pageY-10
+        if(this.selectedPiece == undefined){
+            this.selectedPiece = this.pieces.find(p => checkPos(p.pos,this.convertPos({x:x,y:y})) && p.color == this.turn)
+        }
+        else{
+            const moves = this.selectedPiece.moves(this.pieces,this.moveHistory)
+            const pos = this.convertPos({x:x,y:y})
+            if(moves.find(m => checkPos(m,pos)) != undefined){
+                this.selectedPiece.move(pos,this.pieces,this.moveHistory)
+                this.selectedPiece = undefined
+                this.turn = ['white','black'].find(c => c != this.turn)
+                if(this.turn == 'black'){
+                    setTimeout(() => {
+                        bot.play(this.pieces,this.moveHistory)
+                    },750)
                 }
-                else{
-                    this.selectedPiece = this.pieces.find(p => checkPos(pos,p.pos) && p.color == this.turn)
-                }
+                setTimeout(() => {
+                    if(ifCheckmate(this.turn,this.pieces,this.moveHistory)){
+                        alert(['white','black'].find(c => c != this.turn)+' wins!')
+                    }
+                },500)
             }
             else{
-                this.selectedPiece = this.pieces.find(p => p.color == this.turn && checkPos(pos,p.pos))
+                this.selectedPiece = this.pieces.find(p => checkPos(p.pos,this.convertPos({x:x,y:y})) && p.color == this.turn)
+            }
+        }
+    }
+    convertPos(pos){
+        if(pos.row != undefined){
+            return {x: this.margin+pos.col*this.size/8+this.size/16, y:this.margin+pos.row*this.size/8+this.size/16}
+        }
+        else{
+            const x = pos.x-this.margin
+            const y = pos.y-this.margin
+            if(x>=0 && x<=this.size && y>=0 && y<=this.size){
+                return {row: Math.floor((y/this.size)*8), col: Math.floor((x/this.size)*8)}
+            }
+            else{
+                return {row:100,col:100}
             }
         }
     }
@@ -164,88 +160,111 @@ class Bot{
         this.color = color
     }
     play(state,moveHistory){
-        const {x:X,y:Y,w:w,h:h} = chessBoard
-        const x = X+10+w/16
-        const y = Y+10+h/16
-        let move = this.findMateIn1(state,moveHistory,this.color)
-        if(move != undefined){
-            movePiece(move,move.moveTo,state,moveHistory)
-            setTimeout(() => {alert(this.color+' wins!')},100)
-        }
-        else{
-            const moves = this.eliminateBadMoves(state,moveHistory,this.color)
-            if(moves.length>0){
-                const piece = moves[Math.round(Math.random()*(moves.length-1))]
-                const move = piece.moves[Math.round(Math.random()*(piece.moves.length-1))]
-                movePiece(piece,move,state,moveHistory)
-            }
-            else{
-                const piece = state.find(p => p.color == this.color && getMoves(p,state,moveHistory).length>0)
-                movePiece(piece,getMoves(piece,state,moveHistory)[0],state,moveHistory)
-            }
-        }
-        chessBoard.turn = ['white','black'].find(c => c != this.color)
-    }
-    findMateIn1(state,moveHistory,turn){
-        const opPieces = state.filter(p => p.color == turn)
-        const move = opPieces.find(p => getMoves(p,state,moveHistory).find(m => {
-            const copyState = JSON.parse(JSON.stringify(state))
-            const index = copyState.findIndex(r => checkPos(m,r.pos))
-            const copyMoveHistory = JSON.parse(JSON.stringify(moveHistory))
-            copyMoveHistory.push(new RecordMove(p,m,state))
-            if(index>=0){
-                copyState.splice(index,1)
-            }
-            copyState.find(r => checkPos(r.pos,p.pos)).pos = m
-            if(ifCheckmate(copyState,copyMoveHistory,['white','black'].find(c => c != turn))){
-                p.moveTo = m
-                return true
-            }
-        }))
-        return move
-    }
-    findExchanges(state,moveHistory,turn){
-
-    }
-    eliminateBadMoves(state,moveHistory,turn){
-        return state.filter(p => {
-            if(p.color == turn){
-                p.moves = getMoves(p,state,moveHistory)
-                p.moves.filter(m => {
-                    const copyState = JSON.parse(JSON.stringify(state))
-                    const copyMoveHistory = JSON.parse(JSON.stringify(moveHistory))
-                    const index = copyState.findIndex(r => checkPos(r.pos,m))
-                    if(index>=0){
-                        copyState.splice(index,1)
+        if(ifCheckmate(this.color,state,moveHistory) == false){
+            const pieces = state.filter(p => p.color == this.color && p.moves(state,moveHistory).length>0)
+            const mate = this.findMateIn1(this.color,pieces,state,moveHistory)
+            let piece = mate.piece
+            let move = mate.move
+            if(piece == undefined){
+                const moves = this.avoidMate(pieces,state,moveHistory)
+                if(moves.length==0){
+                    piece = pieces[Math.round(Math.random()*(pieces.length-1))]
+                    move = piece.moves(state,moveHistory)[Math.round(Math.random()*(piece.moves(state,moveHistory).length-1))]
+                }
+                else{
+                    const exchange = this.findEchange(moves,state,moveHistory)
+                    if(exchange != undefined){
+                        piece = exchange[0]
+                        move = exchange[1]
                     }
-                    copyMoveHistory.push(new RecordMove(p,m,copyState))
-                    copyState.find(r => checkPos(r.pos,p.pos)).pos = m
-                    if(this.findMateIn1(copyState,copyMoveHistory,['white','black'].find(c => c != turn))){
-                        p.moves = p.moves.filter(l => checkPos(l,m) == false)
+                    else{
+                        const chosen = moves[Math.round(Math.random()*(moves.length-1))]
+                        piece = chosen[0]
+                        move = chosen[1]
                     }
-                })
-                if(p.moves.length>0){
-                    return true
                 }
             }
-        })
+            let pos = chessboard.convertPos(piece.pos)
+            chessboard.wasClicked({pageX:pos.x,pageY:pos.y})
+            pos = chessboard.convertPos(move)
+            chessboard.wasClicked({pageX:pos.x,pageY:pos.y})
+        }
+    }
+    findMateIn1(color,pieces,state,moveHistory){
+        for(let i=0; i<pieces.length; i++){
+            const moves = getMoves(pieces[i],state,moveHistory)
+            for(let m=0; m<moves.length; m++){
+                const copyState = copyArray(state)
+                const copyMoveHistory = copyArray(moveHistory)
+                const copyPiece = copyState.find(p => checkPos(p.pos,pieces[i].pos))
+                movePiece(copyPiece,moves[m],copyState,copyMoveHistory,false)
+                copyPiece.pos = moves[m]
+                if(ifCheckmate(['white','black'].find(c => c != color),copyState,copyMoveHistory)){
+                    return {piece:pieces[i],move:moves[m]}
+                }
+            }
+        }
+        return {piece:undefined,move:undefined}
+    }
+    avoidMate(pieces,state,moveHistory){
+        const possibleMoves = []
+        for(let i=0; i<pieces.length; i++){
+            const moves = pieces[i].moves(state,moveHistory)
+            for(let m=0; m<moves.length; m++){
+                const copyState = copyArray(state)
+                const copyMoveHistory = copyArray(moveHistory)
+                const copyPiece = copyState.find(p => checkPos(p.pos,pieces[i].pos))
+                movePiece(copyPiece,moves[m],copyState,copyMoveHistory,false)
+                copyPiece.pos = moves[m]
+                if(this.findMateIn1(['white','black'].find(c => c != this.color),copyState.filter(p => p.color != this.color && getMoves(p,copyState,copyMoveHistory).length>0),copyState,copyMoveHistory).piece == undefined){
+                    possibleMoves.push([pieces[i],moves[m]])
+                }
+            }
+        }
+        return possibleMoves
+    }
+    findEchange(moves,state,moveHistory){
+        const exchanges = []
+        const difs = []
+        for(let i=0; i<moves.length; i++){
+            const copyState = copyArray(state)
+            const copyMoveHistory = copyArray(moveHistory)
+            const copyPiece = copyState.find(p => checkPos(p.pos,moves[i][0].pos))
+            const op = copyState.find(p => checkPos(p.pos,moves[i][1]))
+            if(op != undefined){
+                const value = op.value
+                movePiece(copyPiece,moves[i][1],copyState,copyMoveHistory,false)
+                copyPiece.pos = moves[i][1]
+                if(copyState.find(p => p.color != this.color && getMoves(p,copyState,copyMoveHistory).find(m => checkPos(m,copyPiece.pos)) != undefined) == undefined){
+                    exchanges.push(moves[i])
+                    difs.push(value)
+                }
+            }
+        }
+        if(difs.length>0){
+            return exchanges[difs.findIndex(d => d == Math.max(...difs))]
+        }
+        else{
+            return undefined
+        }
     }
 }
-const chessBoard = new ChessBoard(size()/16,size()/16,size()*0.875,size()*0.875)
-const bot1 = new Bot('black')
-const bot2 = new Bot('white')
+const bot = new Bot('black')
+const chessboard = new Chessboard()
 function mainLoop(){
     ctx.fillStyle = '#ffffff'
-    ctx.fillRect(0,0,900,1200)
-    chessBoard.draw()
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    chessboard.draw()
     requestAnimationFrame(mainLoop)
 }
 mainLoop()
 window.addEventListener('resize',function(e){
     canvas.height = e.currentTarget.innerHeight-20
     canvas.width = e.currentTarget.innerWidth-20
-    chessBoard.update()
+    chessboard.updateSize()
 })
 window.addEventListener('mousedown',function(e){
-    chessBoard.wasClicked(e)
+    if(chessboard.pieces.find(p => p.moving == true) == undefined && chessboard.turn == 'white'){
+        chessboard.wasClicked(e)
+    }
 })
